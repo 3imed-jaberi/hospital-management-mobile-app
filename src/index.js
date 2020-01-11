199d5e807e4285a70b59adecdadc7907600fdd85
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-11 15:16:22 +0100

    aadd root src 🥦 ..

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 
-/* root file 🚊 .. */
+/* root naviagtion file 🚊 .. */
+
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { AppStack, AuthStack } from './routes';
+import { AuthLoading } from './screens';
+
+
+
+const root = createSwitchNavigator(
+  {
+    Starter: AuthLoading,
+    App: AppStack,
+    Auth: AuthStack
+  },
+  {
+    initialRouteName: 'Starter'
+  }
+);
+
+
+export default createAppContainer(root) ;
